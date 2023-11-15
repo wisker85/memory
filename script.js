75217061fab780c270c481fc4202a4edea77a683
wisker85 <gripunt@hotmail.com>
2023-11-15 19:18:50 +0100

    implementato il reset e il blocco del board

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const carte = document.querySelectorAll('.carta');
 let cartaGirata = false;
 let primaCarta, secondaCarta;
+let bloccaBoard = false;
 
 function giraCarta() {
+    if(bloccaBoard) return;
+    if(this === primaCarta) return;
+
     this.classList.add('flip');
 
     if(!cartaGirata){
@@ -12,7 +16,6 @@ function giraCarta() {
     }
 
     secondaCarta = this;
-    cartaGirata = false;
 
     controllaCorrispondenza();
 }
@@ -25,15 +28,24 @@ function controllaCorrispondenza(){
 function disabilitaCarte(){
     primaCarta.removeEventListener('click', giraCarta);
     secondaCarta.removeEventListener('click', giraCarta);
+    resetBoard();
     
 }
 
 function rigiraCarte(){
+    bloccaBoard = true;
+
     setTimeout(() => {
         primaCarta.classList.remove('flip');
         secondaCarta.classList.remove('flip');
+        resetBoard();
     },1500)
 
+}
+
+function resetBoard(){
+    [cartaGirata, bloccaBoard] =  [false, false];
+    [primaCarta, secondaCarta] = [null, null];
 }
 
 carte.forEach(carta => carta.addEventListener('click', giraCarta));
