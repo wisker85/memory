162248cea5d113b3958fb23d2d7c85cbf588d840
wisker85 <gripunt@hotmail.com>
2023-11-15 19:09:51 +0100

    logica iniziale per verificare le corrispondenze

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const carte = document.querySelectorAll('.carta');
+let cartaGirata = false;
+let primaCarta, secondaCarta;
 
 function giraCarta() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    if(!cartaGirata){
+        cartaGirata = true;
+        primaCarta = this;
+        return;
+    }
+
+    secondaCarta = this;
+    cartaGirata = false;
+
+    controllaCorrispondenza();
+}
+
+function controllaCorrispondenza(){
+    let corrisponde = primaCarta.dataset.forma === secondaCarta.dataset.forma;
+    corrisponde ? disabilitaCarte() : rigiraCarte();
+}
+
+function disabilitaCarte(){
+    primaCarta.removeEventListener('click', giraCarta);
+    secondaCarta.removeEventListener('click', giraCarta);
+    
+}
+
+function rigiraCarte(){
+    setTimeout(() => {
+        primaCarta.classList.remove('flip');
+        secondaCarta.classList.remove('flip');
+    },1500)
+
 }
 
 carte.forEach(carta => carta.addEventListener('click', giraCarta));
